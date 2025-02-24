2054b7c2ffacc4ef417b8c5bd6a25ae9f0b2e07b
Bakkiya122 <bakkiyalakshmi428@gmail.com>
2025-02-24 23:02:41 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(cors());
 
 app.use(express.json());
 
